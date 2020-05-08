d59284078293da908bf695196b38bde4b5bacf2c
hamadichihaoui <chihaoui.hamadi@gmail.com>
2020-05-08 12:23:08 +0300

    implement Region Proposal Network

--- models/rpn.py
+++ models/rpn.py
@@ -2,30 +2,94 @@
 # Written by Hamadi Chihaoui at 11:54 AM 5/7/2020
 # --------------------------------------------------------
 
+from torch.nn import functional as F
 from torch import nn
+from utils.utils import *
+
 
 class RPN(nn.Module):
 
-    def __init__(self, features_channels=512, intermediate_channels=512, n_anchor=9):
-        super.RPN.__init__()
+    def __init__(self, features_channels=512, intermediate_channels=512, n_anchor=9, nms_thresh=0.7):
+        super(RPN, self).__init__()
         self.features_channels = features_channels
         self.intermediate_channels = intermediate_channels
         self.n_anchor = n_anchor
-        self.conv1 = nn.Conv2d(in_channels=self.features_channels, out_channels=self.intermediate_channels, kernel_size=3, stride=1, padding=1)
-        self.conv2_obj = nn.Conv2d(in_channels=self.intermediate_channels, out_channels=2 * self.n_anchor, kernel_size=1, stride=1, padding=1)
-        self.conv2_loc = nn.Conv2d(in_channels=self.intermediate_channels, out_channels=4 * self.n_anchor, kernel_size=1, stride=1, padding=1)
+        self.nms_thresh = nms_thresh
+        self.base_anchor = generate_base_anchor()
+        self.conv1 = nn.Conv2d(in_channels=self.features_channels, out_channels=self.intermediate_channels,
+                               kernel_size=3, stride=1, padding=1)
+        self.objectness = nn.Conv2d(in_channels=self.intermediate_channels, out_channels=2 * self.n_anchor,
+                                    kernel_size=1, stride=1, padding=1)
+        self.localization = nn.Conv2d(in_channels=self.intermediate_channels, out_channels=4 * self.n_anchor,
+                                      kernel_size=1, stride=1, padding=1)
+
+    def _generate_proposals_single_image(self, localization_scores_single_image, rpn_fg_scores_single_image, anchors, img_size, n_post_nms=300):
+        n_pre_nms = 6000
+        roi = loc2bbox(localization_scores_single_image, anchors)
+
+        # clip predicted bbox to image
+        roi[:, slice(0, 4, 2)] = np.clip(roi[:, slice(0, 4, 2)], 0, img_size[0])
+        roi[:, slice(1, 4, 2)] = np.clip(roi[:, slice(1, 4, 2)], 0, img_size[1])
+
+        # remove/filter bbox less than minimum size
+        min_size = self.min_size
+        hs = roi[:, 2] - roi[:, 0]
+        ws = roi[:, 3] - roi[:, 1]
+        keep = np.where((hs >= min_size) & (ws >= min_size))[0]
+        roi = roi[keep, :]
+        rpn_fg_scores_single_image = rpn_fg_scores_single_image[keep]
+
+        # sort all pair (proposal, score) by score from highest to lowest
+        order = rpn_fg_scores_single_image.ravel().argsort()[::-1]
+
+        if n_pre_nms > 0:
+            order = order[:n_pre_nms]
+        roi = roi[order, :]
+        roi = non_maximum_suppression(roi, self.nms_thresh)
+        if n_post_nms > 0:
+            roi = roi[:n_post_nms]
+
+        return roi
+
+    def _generate_proposals(self, localization_scores, rpn_fg_scores, anchors, img_size, n_post_nms=300):
+        """
+
+        :param localization_scores: #shape b, h * w * n_anchor, 4
+        :param rpn_fg_scores: #shape b, h * w * n_anchor
+        :param anchors: #shape h * w * n_anchor, 4
+        """
+
+        localization_scores = localization_scores.cpu().data.numpy()
+        rpn_fg_scores = rpn_fg_scores.cpu().data.numpy()
+        rois = []
+        roi_indices = []
+        for i in range(rpn_fg_scores.shape[0]):
+            roi = self._generate_proposals_single_image(localization_scores[i], rpn_fg_scores[i], anchors, img_size, n_post_nms)
+            batch_index = i * np.ones((len(roi),), dtype=np.int32)
+            rois.append(roi)
+            roi_indices.append(batch_index)
+
+        return rois, roi_indices
+
+    def forward(self, x, img_size):
+        b, c, h, w = x.size()
+        anchors = generate_anchors(self.base_anchor, h, w, feat_stride=16)
+        n_anchor = anchors.shape[0] // (h * w)
+        features = self.conv1(x)
+        rpn_objectness_scores = self.objectness(features)
+        rpn_objectness_scores = rpn_objectness_scores.permute(0, 2, 3, 1)
+        probs = F.softmax(rpn_objectness_scores.view(b, h, w, n_anchor, 2), dim=4)
+        rpn_fg_scores = probs[:, :, :, :, 1]
+        rpn_fg_scores = rpn_fg_scores.view(b, -1)
+        rpn_objectness_scores = rpn_objectness_scores.view(b, -1, 2)
+        rpn_localization = self.localization(features)
+        rpn_localization = rpn_localization.permute(0, 2, 3, 1).contiguous().view(b, -1, 4)
+        rois, roi_indices = self._generate_proposals(rpn_localization, rpn_fg_scores, anchors, img_size)
+
+        return rpn_objectness_scores, rpn_localization,  rois, roi_indices, anchors
 
 
-    def _generate_proposals(self):
-        raise NotImplemented
 
-    def forward(self, x):
-        h = self.conv1(x)
-        objectness_scores = self.conv2_obj(h)
-        localization_scores = self.conv2_loc(h)
-        proposals = self._generate_proposals()
-
-        return proposals, objectness_scores, localization_scores
-
-
-
+if __name__ == '__main__':
+    rpn = RPN()
+    #rpn.generate_anchors(60,40,16)
---
+++ utils/utils.py
@@ -0,0 +1,160 @@
+# --------------------------------------------------------
+# Written by Hamadi Chihaoui at 8:59 AM 5/8/2020 
+# --------------------------------------------------------
+import numpy as np
+import cv2
+
+
+def loc2bbox(src_bbox, locs):
+    """
+
+    :param src_bbox: #shape h * w * n_anchor, 4
+    :param locs: #shape  h * w * n_anchor, 4
+    """
+    import numpy as np
+    src_height = src_bbox[:, 2] - src_bbox[:, 0]
+    src_width = src_bbox[:, 3] - src_bbox[:, 1]
+    src_ctr_y = src_bbox[:, 0] + 0.5 * src_height
+    src_ctr_x = src_bbox[:, 1] + 0.5 * src_width
+
+    dy = locs[:, 0]
+    dx = locs[:, 1]
+    dh = locs[:, 2]
+    dw = locs[:, 3]
+
+    ctr_y = dy * src_height[:, np.newaxis] + src_ctr_y[:, np.newaxis]
+    ctr_x = dx * src_width[:, np.newaxis] + src_ctr_x[:, np.newaxis]
+    h = np.exp(dh) * src_height[:, np.newaxis]
+    w = np.exp(dw) * src_width[:, np.newaxis]
+
+    dst_bbox = np.concatenate([ctr_y - 0.5 * h, ctr_x - 0.5 * w, ctr_y + 0.5 * h, ctr_x + 0.5 * w], axis=-1)
+    return dst_bbox
+
+
+def intersection_over_union(bbox_a, bboxes):
+    x1_max = np.maximum(bbox_a[0], bboxes[:, 0])
+    y1_max = np.maximum(bbox_a[1], bboxes[:, 1])
+    x2_min = np.minimum(bbox_a[2], bboxes[:, 2])
+    y2_min = np.minimum(bbox_a[3], bboxes[:, 3])
+
+    intersection_w = np.maximum(x2_min - x1_max, 0.)
+    intersection_h = np.maximum(y2_min - y1_max, 0.)
+    intersection_area = intersection_w * intersection_h
+
+    area_bbox_a = (bbox_a[2] - bbox_a[0]) * (bbox_a[3] - bbox_a[1])
+    area_bbox_b = (bboxes[:, 2] - bboxes[:, 0]) * (bboxes[:, 3] - bboxes[:, 1])
+
+    iou = intersection_area / (area_bbox_a + area_bbox_b - intersection_area)
+
+    return iou
+
+def non_maximum_suppression(bboxes, threshold, confidence_scores=None):
+    # x1= bboxes[:, 0]
+    # y1= bboxes[:, 1]
+    # x2= bboxes[:, 2]
+    # y2= bboxes[:, 3]
+    #
+    # areas = (x2 - x1) * (y2 - y1)
+
+    # Picked bounding boxes
+    picked_boxes = []
+    # if confidence_scores is None :
+    picked_scores = []
+
+    # Sort by confidence score of bounding boxes
+    order = np.argsort(confidence_scores)
+    while order.size > 0:
+
+        i = order[-1]
+        print('i ', i)
+        print('order ', order[:-1])
+        print(bboxes[i])
+        # Pick the bounding box with largest confidence score
+        picked_boxes.append(bboxes[i])
+        if confidence_scores is not None:
+            picked_scores.append(confidence_scores[i])
+
+        iou = intersection_over_union(bboxes[i], bboxes[order[:-1], :])
+        keep = np.where(iou > threshold)
+        order = order[keep]
+    if confidence_scores is not None:
+        return picked_boxes, picked_scores
+    else:
+        return picked_boxes
+
+
+def generate_base_anchor(scales=[128, 256, 512], ratios=[0.5, 1, 2]):
+
+    anchor_base = np.zeros(len(scales) * len(ratios), 4).reshape((len(scales), len(ratios), 4))
+    for i in range(len(scales)):
+        scale = scales[i]
+        for j in range(len(ratios)):
+            ratio = ratios[j]
+            w = scale * np.sqrt(ratio)
+            h = scale * np.sqrt(1. / ratio)
+            anchor_base[i, j, 0] = -1 * h / 2.
+            anchor_base[i, j, 1] = -1 * w / 2.
+            anchor_base[i, j, 2] = h / 2.
+            anchor_base[i, j, 3] = w / 2.
+
+    return anchor_base.reshape(len(scales) * len(ratios), 4)  # shape (len(scales) * len(ratios), 4)
+
+
+def generate_anchors(base_anchor, feat_width, feat_height, feat_stride):
+
+    shift_y = np.arange(0, feat_height * feat_stride, feat_stride)
+    shift_x = np.arange(0, feat_width * feat_stride, feat_stride)
+    shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+    shift = np.stack((shift_y.ravel(), shift_x.ravel(), shift_y.ravel(), shift_x.ravel()), axis=1) #shape (feat_height * feat_width, 4)
+    anchors = shift.reshape((1, -1, 4)).transpose((1, 0, 2)) + base_anchor.reshape((1, -1, 4))
+    anchors = anchors.reshape(-1, 4).astype(np.float32)
+
+    return anchors
+
+
+
+if __name__ == '__main__':
+
+    # Image name
+    image_name = '1_684.jpg'
+
+    # Bounding boxes
+    bounding_boxes = np.array([[187, 82, 337, 317], [150, 67, 305, 282], [246, 121, 368, 304], [200, 170, 400, 180]])
+    confidence_score = [0.9, 0.75, 0.8, 0.5]
+
+    # Read image
+    image = cv2.imread(image_name)
+
+    # Copy image as original
+    org = image.copy()
+
+    # Draw parameters
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    thickness = 2
+
+    # IoU threshold
+    threshold = 0.7
+
+    # Draw bounding boxes and confidence score
+    for (start_x, start_y, end_x, end_y), confidence in zip(bounding_boxes, confidence_score):
+        (w, h), baseline = cv2.getTextSize(str(confidence), font, font_scale, thickness)
+        cv2.rectangle(org, (start_x, start_y - (2 * baseline + 5)), (start_x + w, start_y), (0, 255, 255), -1)
+        cv2.rectangle(org, (start_x, start_y), (end_x, end_y), (0, 255, 255), 2)
+        cv2.putText(org, str(confidence), (start_x, start_y), font, font_scale, (0, 0, 0), thickness)
+
+    # Run non-max suppression algorithm
+    picked_boxes, picked_score = non_maximum_suppression(bounding_boxes,threshold, confidence_score)
+
+    # Draw bounding boxes and confidence score after non-maximum supression
+    for (start_x, start_y, end_x, end_y), confidence in zip(picked_boxes, picked_score):
+        (w, h), baseline = cv2.getTextSize(str(confidence), font, font_scale, thickness)
+        cv2.rectangle(image, (start_x, start_y - (2 * baseline + 5)), (start_x + w, start_y), (0, 255, 255), -1)
+        cv2.rectangle(image, (start_x, start_y), (end_x, end_y), (0, 255, 255), 2)
+        cv2.putText(image, str(confidence), (start_x, start_y), font, font_scale, (0, 0, 0), thickness)
+
+    # Show image
+    cv2.imshow('Original', org)
+    cv2.imshow('NMS', image)
+    cv2.waitKey(0)
+
